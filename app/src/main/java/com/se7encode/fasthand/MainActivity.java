429d0a7336b2c1ff06e6b06014acfc50b5668d6e
nightlight9 <yazhoufeilong@gmail.com>
2017-01-23 10:10:48 +0800

    Initial commit

---
+++ app/src/main/java/com/se7encode/fasthand/MainActivity.java
@@ -0,0 +1,15 @@
+package com.se7encode.fasthand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle(getResources().getString(R.string.app_title));
+
+    }
+}
---
+++ app/src/main/java/com/se7encode/fasthand/service/RedBagFightService.java
@@ -0,0 +1,22 @@
+package com.se7encode.fasthand.service;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Author:          Leo
+ * Version          V1.0
+ * Date:            2017/1/22.
+ * Description:     抢红包服务
+ */
+public class RedBagFightService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
